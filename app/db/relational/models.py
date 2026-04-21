e351bb7f152498967eeab992c67dbaf8cde5696a
byc <byckg3@outlook.com>
2026-04-22 01:05:22 +0800

    feat(models): add type annotation map and updated_at field to models

--- app/db/relational/models.py
+++ app/db/relational/models.py
@@ -1,22 +1,26 @@
 from datetime import datetime
+from typing import Any
 
 from sqlalchemy import BigInteger, DateTime, func
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 # avoid circular dependency
 class Base( AsyncAttrs, DeclarativeBase ):
-    pass
+    type_annotation_map = {
+        dict[ str, Any ]: JSONB
+    }
 
 class IdMixin:
     id: Mapped[ int ] = mapped_column( BigInteger, primary_key = True )
 
 class TimestampMixin:
-    created_at: Mapped[ datetime ] = mapped_column( DateTime( timezone = True ), 
+    created_at: Mapped[ datetime ] = mapped_column( DateTime( timezone = True ),
                                                     server_default = func.now() )
-    
-    updated_at: Mapped[ datetime ] = mapped_column( DateTime( timezone = True ), 
-                                                    server_default = func.now(), 
+
+    updated_at: Mapped[ datetime ] = mapped_column( DateTime( timezone = True ),
+                                                    server_default = func.now(),
                                                     onupdate = func.now() )
 
 
@@ -24,10 +28,10 @@ class BaseEntity( Base ):
     __abstract__ = True
 
     id: Mapped[ int ] = mapped_column( BigInteger, primary_key = True )
-   
-    created_at: Mapped[ datetime ] = mapped_column( DateTime( timezone = True ), 
+
+    created_at: Mapped[ datetime ] = mapped_column( DateTime( timezone = True ),
                                                     server_default = func.now() )
-    
-    updated_at: Mapped[ datetime ] = mapped_column( DateTime( timezone = True ), 
-                                                    server_default = func.now(), 
+
+    updated_at: Mapped[ datetime ] = mapped_column( DateTime( timezone = True ),
+                                                    server_default = func.now(),
                                                     onupdate = func.now() )
